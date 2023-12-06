05bd543959d6c3368553b19fed97fec9b8755ab3
gustafgroning <gustaf.groning@sigicom.com>
2023-12-06 10:42:46 +0100

    worked on RegisterDogScreen

--- petology/src/screens/RegisterDogScreen.js
+++ petology/src/screens/RegisterDogScreen.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import {
   View,
-  Text,
-  TextInput,
   StyleSheet,
+  TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import { Button } from "react-native-paper";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import { Button, Text } from "react-native-paper";
 // import { Picker } from "@react-native-picker/picker"; Fix this for hane/tik
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import DropDownPicker from 'react-native-dropdown-picker';
+
 
 const RegisterDogScreen = () => {
   const [name, setName] = useState("");
@@ -17,6 +18,15 @@ const RegisterDogScreen = () => {
   const [birthday, setBirthday] = useState(new Date());
   const [sex, setSex] = useState("Hane");
   const [showDatePicker, setShowDatePicker] = useState(false);
+
+
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
   /**************************************************************/
 
   const handleCreateDog = async () => {
@@ -58,36 +68,111 @@ const RegisterDogScreen = () => {
   };
   /**************************************************************/
 
+  // return (
+    // <View style={styles.container}>
+    //   <Text>Petology</Text>
+    //   <TextInput placeholder="Fido" value={name} onChangeText={setName} />
+    //   <TextInput placeholder="Labrador" value={breed} onChangeText={setBreed} />
+    //   <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+    //     <Text>{birthday.toLocaleDateString()}</Text>
+    //   </TouchableOpacity>
+    //   {showDatePicker && (
+    //     <DateTimePicker
+    //       value={birthday}
+    //       mode="date"
+    //       display="default"
+    //       onChange={onDateChange}
+    //     />
+    //   )}
+    //   <TextInput placeholder="Tik" value={sex} onChangeText={setSex} />
+    //   {/* TODO: turn into ENUM, 1 = hane, 2 = tik */}
+    //   <Button onPress={handleCreateDog}>Lägg till hund</Button>
+    // </View>
+
+  // );
   return (
-    <View style={styles.container}>
-      <Text>Petology</Text>
-      <TextInput placeholder="Fido" value={name} onChangeText={setName} />
-      <TextInput placeholder="Labrador" value={breed} onChangeText={setBreed} />
-      <TouchableOpacity onPress={() => setShowDatePicker(true)}>
-        <Text>{birthday.toLocaleDateString()}</Text>
-      </TouchableOpacity>
-      {showDatePicker && (
-        <DateTimePicker
-          value={birthday}
-          mode="date"
-          display="default"
-          onChange={onDateChange}
-        />
-      )}
-      <TextInput placeholder="Tik" value={sex} onChangeText={setSex} />
-      {/* TODO: turn into ENUM, 1 = hane, 2 = tik */}
-      <Button onPress={handleCreateDog}>Lägg till hund</Button>
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <View style={styles.section1}> 
+        <Text variant="headlineLarge" style={styles.header}>
+            Petology
+          </Text>
+        </View> 
+        <View style={styles.section2}> 
+        <Text variant="headlineMedium" style={styles.headerSmall}>
+            Skapa hund
+        </Text>
+        <TextInput style={styles.FormInput} placeholder="Namn" placeholderTextColor={'black'}/>
+        <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+    />
+        <TextInput style={styles.FormInput} placeholder="Namn" placeholderTextColor={'black'}/>
+        <TextInput style={styles.FormInput} placeholder="Namn" placeholderTextColor={'black'}/>
+        </View>
+        <View style={styles.section} />
+        <View style={styles.section} />
+        {/* Add more sections as needed */}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  scrollView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
+    backgroundColor: "#92cdca",
   },
+  container: {
+    // flex: 1,
+  },
+  section1: {
+    height: 150,
+    borderWidth: 1,
+    borderColor: 'black', 
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  section2: {
+    height: 400,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingTop: 10,
+  },
+  section: {
+    height: 300,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  header: {
+    fontFamily: "Cochin",
+  },
+  headerSmall: {
+    fontFamily: "Cochin",
+    marginBottom: 15,
+  },
+  formButtons: {
+    width: '80%',
+    backgroundColor: '#e8f5f5',
+    borderColor: 'black',  
+    marginBottom: 10,
+  },
+  FormInput: {
+    width: '80%',
+    height: '10%',
+    backgroundColor: '#e8f5f5',
+    borderColor: 'grey',  
+    borderWidth: 1,
+    borderRadius: 90,
+    marginBottom: 10,
+    textAlign: 'center',
+  }
+  
 });
 
 export default RegisterDogScreen;
